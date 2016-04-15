380d91bc704e561029bc600be0734d7483d9fe45
ryanhnelson <sublimity@gmail.com>
2016-04-15 12:22:42 +0200

    Update
    Working further, answering Advertising questions on book pg. 20

--- chapter-2-statistical-learning.R
+++ chapter-2-statistical-learning.R
@@ -7,6 +7,7 @@ library(tidyr)
 library(ggplot2)
 
 ### Advertising dataset
+setwd("C:/Users/rnelson020/Documents/GitHub/introduction-statistical-learning/datasets/")
 Advertising <- read.csv("Advertising.csv")
 utils::View(Advertising)
 qplot(data = Advertising, x = TV, y = Sales, geom=c("point", "smooth"))
@@ -17,7 +18,9 @@ lm_tv <- lm(TV ~ Sales, data = Advertising)  ##  Example linear regressions
 lm_radio <- lm(Radio ~ Sales, data = Advertising)
 lm_newspaper <- lm(Newspaper ~ Sales, data = Advertising)
 
-summary(lm_tv)  ##  Multiple R-squared: 0.6119
+summary(lm_tv) # Multiple R-squared: 0.6119
+summary(lm_radio) # Multiple R-squared: 0.332
+summary(lm_newspaper) # Multiple R-squared: 0.05212
 
 ### Income dataset
 Income <- read.csv("Income2.csv")  ##  Income1.csv lacks Seniority
@@ -27,6 +30,15 @@ lm_education <- lm(Income ~ Education, data = Income)
 lm_seniority <- lm(Income ~ Seniority, data = Income)
 summary(lm_education)  ##  Multiple R-squared: 0.931
 summary(lm_seniority)  ##  Multiple R-squared: 0.2686
+ # Apparently education makes a huge difference, and seniority not
 
-  ## Next step: 3d plot in ggplot2
-  ## On page 32
+### Page 20: Which media contribute to sales?
+### Which media generate the biggest boost in sales? or
+### How much increase in sales is associated with a given
+  ### increase in TV advertising?
+str(Advertising) # Needs gathering
+Advertising_long <- tidyr::gather(Advertising, Category, Budget, TV:Newspaper)
+qplot(data=Advertising_long, x=Budget, y=Sales,
+      color=Category, geom=c("point", "smooth"))
+ # Answer: from R-squared above, DEFINITELY TV.
+ # Radio has lower R^2, but smoothed line shows steep correlation
